dca6f59dcf5647c71210082fcb2ca70970446783
Ð~Nęxμş <101008031+D-Nexus@users.noreply.github.com>
2022-09-04 16:41:08 -0300

    Delete ciclos.py

--- ciclos.py
+++
@@ -1,6 +0,0 @@
-contador = 1
-
-
-while contador < 10: #Se repite mientras se cumpla su condición
-    print(contador)
-    contador = contador + 1 #Si no aumentaramos el count el ciclo while se repetiria infinitamente
