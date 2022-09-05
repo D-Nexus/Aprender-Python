ffdfa27ddea8d836e61373bc633f021021b57ebe
Ð~Nęxμş <101008031+D-Nexus@users.noreply.github.com>
2022-09-05 20:02:08 -0300

    Add files via upload

--- Python 2/Python desde cero/Asistente.py
+++ Python 2/Python desde cero/Asistente.py
@@ -1,52 +1,29 @@
 import pyttsx3
-import time
+import speech_recognition as sr
+import datetime
+import pywhatkit
+import os
+import yfinance as yf
+import pyjokes
 
-m = True
+#Escuchar nuestro microfono y devuelve el audio como texto para google
 
+def transformar():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 0.8
+        said = r.listen(source)
+        try:
+            print("Yo estoy escuchando")
+            q = r.recognize_google(said, language="es")
+            return q
+        except sr.UnknownValueError:
+            print("Lo lamento no entendi")
+            return "Yo estoy esperando"
+        except sr.RequestError:
+            print("El servicio esta caido")
+            return "Yo estoy esperando"
+        except:
+            return "Yo estoy esperando"
 
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
-# Control the rate. Higher rate = more speed
-engine.setProperty("rate", 150)
-text = "Porfavor ingrese su nombre"
-engine.say(text)
-engine.runAndWait()
-
-while (m == True):
-
-    n = input("Ingrese su nombre: ")
-
-
-    another_text = "Su nombre es"
-    engine.say(another_text + n)
-    engine.runAndWait()
-
-    another_text = "Es correcto ese nombre, confirme porfavor"
-    engine.say(another_text)
-    engine.runAndWait()
-
-    x = True
-
-    while (x == True):
-        respuesta = input("si o no: ").lower()
-
-        if (respuesta == "si"):
-
-            another_text = "Gracias por la información, que tenga un buen día"
-            engine.say(another_text)
-            engine.runAndWait()
-            m = False
-            x = False
-        elif(respuesta == "no"):
-            another_text = "Entonces ingrese su nombre nuevamente "
-            engine.say(another_text)
-            engine.runAndWait()
-            x = False
-        else:
-            another_text = "No se ha ingresado una respuesta valida, intente denuevo"
-            engine.say(another_text)
-            engine.runAndWait()
-
-
-
+transformar()
